00ed2088d158058c8f550153f87884a89ddc2ca5
Mudassir Shahzad <mudassarshahzad14@gmail.com>
2020-06-14 17:24:29 +0500

    assignment5

---
+++ app.js
@@ -0,0 +1,248 @@
+Chapter 1
+alert("Error! Please enter a valid password.")
+ alert("Welcome to JS Land     Happy Codding!")
+alert("Welcome to JS Land...")
+alert("Happy Coding!") <br>
+alert("Prevent this page from creating additional diologe.")
+alert("Hello... i can run JS through my web browser's console.")
+
+Chapter 2
+username = ;
+myname = "ali khan";
+massage = "Hello World"
+alert(massage)
+name ="Ali Khan";
+alert(name)
+age = 18;
+alert(age)
+course = "web development";
+alert(course)
+a = "PIZZA";
+alert(a , a>=4, a--)
+a = "My email address is";
+email = " example@example.com";
+alert(a  + email)
+a = "I am trying to learn from the Book";
+book = " A Smarter way to  learn javaScript";
+alert(a + book)
+ <p>Yah! I can write HTML content through javaScript</p>
+
+Chapter 3
+age = "I am 20 year old";
+alert(age)
+a = 5;
+b = 4;
+alert(a+b)
+a = 5;
+b = 4;
+alert(a-b) 
+a = 5;
+b = 4;
+alert(a*b)
+a = 5;
+b = 4;
+alert(a/b)
+a = 5;
+b = 4;
+alert(a%b)
+
+Task2
+a = 5;
+b = ++a
+c = a + 7
+d = --c
+e = d % 3
+alert(e)
+
+task3
+ticket_cast = "600";
+total_cast_of_5_ticket = ticket_cast * 5;
+alert("Total cast of 5 ticket is " + total_cast_of_5_ticket)
+c = "45"
+f = ((c*9/5) + 32);
+alert(f)
+f = "48"
+c = ((f-32) * 9/5);
+alert(c)
+
+task4
+price_of_item1 = 450
+quantity_of_item1_is = 4
+price_of_item2 = 560
+quantity_of_item2_is = 5
+shiping_charges = 100
+alert("Price of item 1 is " + price_of_item1)
+alert("Quantity of item 2 is " + quantity_of_item1_is)
+alert("Price of item 2 is " + price_of_item2)
+alert("Quantity of item 2 is " + quantity_of_item2_is)
+alert("Shiping Charges is " + shiping_charges)
+total_cast_is = (price_of_item1 * quantity_of_item1_is) +
+(price_of_item2 * quantity_of_item2_is) + shiping_charges
+alert("Total cast is " + total_cast_is)
+
+task5
+student1
+total_marks = 980
+obtain_marks = 780
+percentage = ((obtain_marks * 100) / total_marks)
+alert("Percentage is " + percentage)
+
+task6
+US_Dollors = 10
+Saudi_Riyals = 25
+convert = ((US_Dollors * 104.80) + (Saudi_Riyals * 28))
+alert("Total currency in PRK: " + convert)
+
+task7
+a = 3;
+c = (((a + 5) * 10) / 2)
+alert(c)
+
+task8
+birth_year = 1996
+current_year = 2020
+age = current_year - birth_year
+alert("Your age is: " + age)
+
+task9
+r = 12;
+c = 2 * 3.142 * r
+a = (3.142 * (r**r))
+alert("The circumference is: " + c)
+alert("The area is: " + a)
+
+task10
+favorite_snack = "chocolate chip"
+current_age = 22
+max_age  = 70
+snack_per_day = 4
+total_snack = ((max_age - current_age) * 365.25) * snack_per_day
+alert("Total rest of your life: " + total_snack)
+ 
+task11
+fovorite_snack = prompt(" Enter your favorite snack")
+current_age = prompt(" Enter your current age ")
+max_age = prompt(" Enter your max age")
+snack_per_day = prompt(" Enter your snack per day")
+total_snack = ((max_age - current_age) * 365.25) * snack_per_day
+alert("Total rest of your life: " + total_snack)
+
+
+ Chapter (6-9)
+task1
+a = 10;
+alert("The value of a is: " + a)
+alert("The value of a++ is: " + ++a)
+
+alert("The value of a is: "  +  a)
+alert("The value of a++ is: " + ++a)
+
+alert("The value of a is: " + a)
+alert( "The value of --a is: "  + --a)
+
+alert("The value of a is: " + a)
+alert( "The value of --a is: "  + --a)
+
+Task2
+a = prompt("Enter your name")
+b = prompt("Enter your greet")
+
+task3
+d = prompt("Enter a number") {
+    for(i = 1; i<=10; i++);
+    alert(d * i = "")
+}
+
+task4
+a = english;
+b = Math;
+c = computer;
+total = 100;
+obtain_marks = ""
+
+
+Chapter9-11
+task1
+var city = prompt("Enter a city");
+if (city === "karachi") {
+    alert("Welcome to karachi");
+}
+else{
+    alert("Welcome to other city.....");
+}
+
+task2
+var gender = prompt("Enter your gender");
+if (gender === "male") {
+    alert("Good morning Sir!!!");
+}
+else {
+    alert("Good morning Ma'am!!");
+}
+
+task3
+var signal = prompt("Enter a signal");
+if (signal === "red") {
+    alert("must stop");
+}
+else if(signal === "yellow") {
+    alert("read to move");
+}
+else {
+    alert("move now");
+}
+
+task4
+var fuel = prompt("Enter a fuel");
+if (fuel < "0.25liters") {
+    alert("Please refil the fuel in your car");
+}
+else{
+    alert("Welcome!");
+}
+
+task5
+a = prompt("enter your marks")
+if (a >= 80) {
+    alert( "Your grade is A+" );
+}
+else if (a >= 70) {
+    alert( "Your grade is A" );
+}
+else if (a >= 60) {
+    alert( "Your grade is B" );
+}
+else if (a >= 50) {
+    alert( "Your grade is C" );
+}
+else if (a >= 40) {
+    alert( "Your grade is D" );
+}
+else{
+    alert("Your are  fail")
+}
+
+task6
+a = prompt("enter your marks")
+if (a > 40) {
+   alert( "It is too hot outside." );
+}
+else if (a > 30) {
+   alert( "The Weather today is Normal." );
+}
+else if (a > 20) {
+     alert( "Today’s Weather is cool." );
+}
+else {
+  alert( "OMG! Today’s weather is so Cool." );
+ }
+
+task7
+a = prompt("first_number")
+b = prompt("operator")
+c = prompt("second_number")
+alert(first_number + operation + second_number)
+
+
+
+
